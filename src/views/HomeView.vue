3d6d3f23b0645f1d763844625f14bc86669d6a38
yongshengss <2973353245@qq.com>
2022-05-06 17:33:44 +0800

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <h3>hello</h3>
+    <el-button type="primary" class="btn">Primary</el-button>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "HomeView",
+  components: {},
+};
+</script>
+<style lang="scss" scoped>
+.home{
+  --button-bk-color:#f00;
+  h3{
+    color:red;
+  }
+  .btn{
+    background: var(--button-bk-color);
+  }
+}
+</style>
